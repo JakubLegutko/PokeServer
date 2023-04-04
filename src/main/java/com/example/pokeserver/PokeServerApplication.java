1f33b1359b6c5117c10f1beffc0ad5d021db1a4b
jlegutko@student.agh.edu.pl <jlegutko@student.agh.edu.pl>
2023-04-04 08:57:12 +0200

    Init

---
+++ src/main/java/com/example/pokeserver/PokeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pokeserver/PokeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
