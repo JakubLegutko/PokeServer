28ceb65e7033ce4bf5a403a228034b9eaa31c0a0
jlegutko@student.agh.edu.pl <jlegutko@student.agh.edu.pl>
2023-04-22 12:32:26 +0200

    ClassicController created

--- src/main/java/com/example/pokeserver/web/ClassicController.java
+++ src/main/java/com/example/pokeserver/web/ClassicController.java
@@ -1,9 +1,10 @@
 package com.example.pokeserver.web;
 
-import ch.qos.logback.core.model.Model;
+
 import com.example.pokeserver.business.UserService;
 import com.example.pokeserver.data.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,7 +19,7 @@ public class ClassicController {
     }
 
     @RequestMapping(path = "/users", method = RequestMethod.GET)
-    public List<User> getUsers(Model model) {
+    public String getUsers(Model model) {
         List<User> users = this.userService.getUsers();
         model.addAttribute("users", users);
         return "users";
