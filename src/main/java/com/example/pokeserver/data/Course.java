3b82c88a6248d58533ce9a8e37baeaad1c6547df
Decane <nightcyberhunter@gmail.com>
2023-06-25 09:49:56 +0200

    Course type correction

--- src/main/java/com/example/pokeserver/data/Course.java
+++ src/main/java/com/example/pokeserver/data/Course.java
@@ -2,7 +2,12 @@ package com.example.pokeserver.data;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Course {
-
+    String id;
+    String title;
+    List<String> taskIds;
+    String imageSrc;
 }
